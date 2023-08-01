c46377bfe21f0fd0fb6f6ae7ef68d0c27271ddc5
Angel Vasilev <angavasilev@gmail.com>
2023-08-01 08:56:58 +0300

    work on footer and basic folder structure

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
     pathMatch: 'full'
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
