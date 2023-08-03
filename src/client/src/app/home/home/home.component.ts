06e143fe6c6ebd3e6d8df35d0dbb299ad4882d17
Angel Vasilev <angavasilev@gmail.com>
2023-08-03 18:50:11 +0300

    create header and footer

--- src/client/src/app/home/home/home.component.ts
+++ src/client/src/app/home/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gains-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    
+  ]
 })
 export class HomeComponent {
 
