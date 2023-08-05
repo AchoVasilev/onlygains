a2d3884101a7211c77eb3434cdd8d9949fa79e56
Angel Vasilev <angavasilev@gmail.com>
2023-08-05 08:34:46 +0300

    add initial home page

--- src/client/src/app/shared/shared.module.ts
+++ src/client/src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CardComponent } from './card/card.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarouselComponent,
+    CardComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    CommonModule,
+    CarouselComponent
   ]
 })
 export class SharedModule { }
