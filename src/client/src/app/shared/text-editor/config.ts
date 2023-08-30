f19947fe27528daf8c5a450f1882a09afefd42ad
Angel Vasilev <angavasilev@gmail.com>
2023-08-31 00:04:30 +0300

    work on tinymce integration

--- src/client/src/app/shared/text-editor/config.ts
+++ src/client/src/app/shared/text-editor/config.ts
@@ -1,12 +1,27 @@
 export interface PostConfig {
-  selector: 'textarea#post';
-  height: 500;
-  menubar: true;
-  language: 'bg_BG';
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table paste code help wordcount'
-  ];
-  toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
+  base_url: string;
+  suffix: string;
+  selector?: string;
+  height?: number;
+  menubar?: boolean;
+  language?: string;
+  language_url?: string;
+  plugins?: string[];
+  toolbar?: string;
+  promotion: boolean;
+  branding: boolean;
 }
+
+export const defaultPostConfig: PostConfig = {
+  base_url: '/tinymce',
+  suffix: '.min',
+  height: 500,
+  menubar: true,
+  language: 'bg_BG',
+  language_url: 'assets/langs/bg_BG.js',
+  toolbar:
+    'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+  promotion: false,
+  branding: false,
+  plugins: ['anchor', 'link', 'lists', 'image',]
+};
