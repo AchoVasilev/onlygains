d7cf7b62f42d6f0b6df5a454cf2ec3aeab2c8c05
Angel Vasilev <angavasilev@gmail.com>
2023-08-31 23:45:07 +0300

    add post creation preview

--- src/client/src/app/features/content-creation/create-post/create-post.component.ts
+++ src/client/src/app/features/content-creation/create-post/create-post.component.ts
@@ -1,6 +1,7 @@
-import { PostConfig, defaultPostConfig } from '../../../shared/text-editor/config';
-import { Component, OnInit } from '@angular/core';
+import { PostConfig, defaultPostConfig } from 'app/shared/text-editor/config';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { threeImageTemplateStyling } from 'app/shared/text-editor/template-stylings';
+import { Editor } from 'tinymce';
 import { threeImageTemplate } from 'app/shared/text-editor/templates';
 
 @Component({
@@ -9,12 +10,13 @@ import { threeImageTemplate } from 'app/shared/text-editor/templates';
   styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent implements OnInit {
-
+  @ViewChild('preview', {static: true}) preview?: ElementRef;
   config: PostConfig = defaultPostConfig;
   strTemplate?: string;
   source: string = '';
+  editor?: Editor;
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
     this.config.content_style = threeImageTemplateStyling;
     this.config.images_upload_handler = this.onUpload;
     this.strTemplate = this.source = threeImageTemplate;
@@ -24,10 +26,17 @@ export class CreatePostComponent implements OnInit {
   }
 
   onEditorInit(event: any) {
-    console.log(event.editor.getContent())
+    this.editor = event.editor;
+    console.log(this.editor?.dom.select('h2')[0].textContent)
+
+    this.renderer.setProperty(this.preview?.nativeElement, 'innerHTML', this.source);
   }
 
   onUpload(blobInfo: any) {
     console.log(blobInfo.blob());
+  }
+
+  onModelChange() {
+    this.renderer.setProperty(this.preview?.nativeElement, 'innerHTML', this.source);
   }
 }
