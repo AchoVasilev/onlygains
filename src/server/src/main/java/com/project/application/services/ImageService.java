12517837b4fb7dffb6cf91acde99ae38cd68ef42
Angel Vasilev <angavasilev@gmail.com>
2023-09-08 08:05:03 +0300

    working file upload

--- src/server/src/main/java/com/project/application/services/ImageService.java
+++ src/server/src/main/java/com/project/application/services/ImageService.java
@@ -8,6 +8,8 @@ import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Singleton
@@ -20,18 +22,36 @@ public class ImageService {
     }
 
     public String upload(String folder, CompletedFileUpload file) {
+        final var rootFolderName = "onlygains";
+        final var uploadFolder = String.format("%s/%s", rootFolderName, folder);
         try {
+            var uploadedFile = this.convertMultipartToFile(file);
             var params = ObjectUtils.asMap(
-                    "public_id", String.format("%s/%s", folder, file.getName())
+                    "public_id", String.format("%s/%s", uploadFolder, uploadedFile.getName())
             );
-            var uploadResult = this.cloudinary.uploader().upload(file, params);
+            var uploadResult = this.cloudinary.uploader().upload(uploadedFile, params);
             var url = uploadResult.get("secure_url").toString();
 
             log.info("Successfully uploaded image. [name={}]", file.getName());
+
+            boolean isDeleted = uploadedFile.delete();
+            if (isDeleted) {
+                log.info("File successfully deleted");
+            } else
+                log.info("File doesn't exist");
 
             return url;
         } catch (IOException e) {
             throw new FileUploadException(file.getClass(), e);
         }
+    }
+
+    private File convertMultipartToFile(CompletedFileUpload file) throws IOException {
+        var convFile = new File(file.getName());
+        var fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+
+        return convFile;
     }
 }
