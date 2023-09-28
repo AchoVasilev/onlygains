a632e10ab874f7206a1c2ad7d830b51c10a57996
Angel Vasilev <angavasilev@gmail.com>
2023-09-28 07:44:59 +0300

    work on muscle selection

--- src/client/src/app/shared/stand-alone/muscle-group/muscle-group.component.ts
+++ src/client/src/app/shared/stand-alone/muscle-group/muscle-group.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,18 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './muscle-group.component.html',
-  styleUrls: ['./muscle-group.component.scss']
+  styleUrls: ['./muscle-group.component.scss'],
 })
-export class MuscleGroupComponent {
+export class MuscleGroupComponent implements OnInit {
+  mainGroups = ['Deltoids'];
+  synergists = ['Rear-deltoids', 'Abs', 'Obliques', 'Chest', 'Triceps'];
 
+  ngOnInit(): void {
+    document.querySelectorAll('.muscle-groups svg g g[id]').forEach((gr) => {
+      if (this.mainGroups.includes(gr.id)) {
+        let children = Array.from(gr.children);
+        children.forEach(child => child.classList.add('red'));
+      }
+    });
+  }
 }
